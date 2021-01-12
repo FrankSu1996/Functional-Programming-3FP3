c1f528fc3b8d4a4832d7fac0f8cb39e714e0dec6
Frank Su <franksu49@gmail.com>
2021-01-12 14:22:13 -0500

    Jan12 lesson code

---
+++ Jan12.hs
@@ -0,0 +1,20 @@
+-- Example code for Jan12
+module Jan12 where
+
+    -- let's play with lists a little
+    -- recall
+    -- data [] a = [] | a : [a]
+    map' :: (a -> b) -> [a] -> [b]
+    map' f [] = []
+    map' f (x : xs) = f x : map' f xs
+
+    filter' :: (a -> Bool) -> [a] -> [a]
+    filter' p [] = []
+    filter' p (x : xs) | p x = x : filter' p xs | otherwise = filter' p xs
+
+    isEven :: Integer -> Bool
+    isEven x = x `mod` 2 == 0
+
+    -- if we want to print something
+    main :: IO ()
+    main = print "Hellworld!"
